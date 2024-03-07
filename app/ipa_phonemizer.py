1ec15b2dd3ff22dba90ffb438358d3dbcc1f8e50
daschablume <daschablume@gmail.com>
2024-03-07 10:56:32 +0100

    added functionality for test button: user can provide their input as well!

--- app/ipa_phonemizer.py
+++ app/ipa_phonemizer.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 import re
 
+from mfa_g2p.generator import PyniniWordListGenerator
+
 PUNCTUATION_STR = r"[!,.\"#$%&\(\)*+:;<=>?@^_`\{|\}~]"
 PUNCT_PATTERN = re.compile(r"\s+(?=" + PUNCTUATION_STR + ")")
 TOK_PATTERN = re.compile(f"\w+'?\w*|{PUNCTUATION_STR}")
@@ -11,7 +13,7 @@ def tokenize(text: str) -> list[str]:
     return re.findall(TOK_PATTERN, text.lower())
 
 
-def convert_text_to_phonemized(input_str, g2p):
+def convert_text_to_phonemized(input_str: str, g2p: PyniniWordListGenerator):
     '''
     Converts text to a phonetized text.
     Returns three variables: 
@@ -25,11 +27,11 @@ def convert_text_to_phonemized(input_str, g2p):
         if not word.isalpha():
             word2phonemized[word].append(word)
         else:
-            phonemized = g2p.rewriter(word)
-            phonemized = set(pho.replace(" ", "") for pho in phonemized)
-            word2phonemized[word] = list(phonemized.union(word2phonemized[word]))
-    
-    # join phonemes into one list: one phoneme per word;
+            word2phonemized[word], _ = get_all_phonemes(
+                g2p, word, set(word2phonemized[word])
+            )
+
+    # join phonemes into one dict: one phoneme per word;
     word2picked_phoneme = {}
     phonemes = []
     for word in word_list:
@@ -41,12 +43,26 @@ def convert_text_to_phonemized(input_str, g2p):
             break
         word2picked_phoneme[word] = phoneme
         phonemes.append(phoneme)
-    
-    # remove whitespaces before punctuation and join the phonemes
+
     phonemized_str = phonemes_to_string(phonemes)
     
     return phonemized_str, word2phonemized, word2picked_phoneme
 
 
 def phonemes_to_string(phonemes: list[str]) -> str:
+    # removes whitespaces before punctuation and joins the phonemes
     return re.sub(PUNCT_PATTERN, "", " ".join(phonemes))
+
+
+def collapse_whitespaces(phoneme: str) -> str:
+    # 'h ɛ l l o ʊ' => 'hɛlloʊ'
+    return phoneme.replace(" ", "")
+
+
+def get_all_phonemes(
+    g2p: PyniniWordListGenerator, word: str, current_phonemes: set
+) -> tuple[list, list]:
+    # joins current phonemes for a word with the output of G2P model
+    phonemized = g2p.rewriter(word)
+    phonemized = [collapse_whitespaces(pho) for pho in phonemized]
+    return list(current_phonemes.union(phonemized)), phonemized
--- app/views.py
+++ app/views.py
@@ -1,46 +1,39 @@
 import json
 import time
 
-from flask import render_template, request, send_from_directory, redirect
+from flask import render_template, request
 
-from ipa_phonemizer import convert_text_to_phonemized, phonemes_to_string
+from ipa_phonemizer import (
+    convert_text_to_phonemized, phonemes_to_string,
+    collapse_whitespaces, get_all_phonemes
+)
 from load_models import G2P
 from matcha_utils import synthesize_matcha_audio
 
 
-def text_to_audio():
+def text_to_audio_view():
     if request.method == 'POST':
         form = request.form
         text = form['text']
+
         if form.get('generate'):
-            phonemized, word2phonemized, word2picked_phoneme = convert_text_to_phonemized(text, G2P)
+            phonemized, word2phonemized, word2picked_phoneme = (
+                convert_text_to_phonemized(text, G2P)
+            )
             
         elif form.get('regenerate'):
-            # it would have been nicer to get word2phonemized directly from the form, 
-            # but json.loads() doesn't give a right encoding for IPA, 
-            # and there is not enought time to fix it.
-            _, word2phonemized, _ = convert_text_to_phonemized(text, G2P)
-            word2picked_phoneme = {}
-            for word in word2phonemized:
-                if not word.isalpha():
-                    continue
-                phonemes = form.getlist(word)
-                picked, manual = phonemes
-                if not manual:          
-                    word2picked_phoneme[word] = picked
-                else:
-                    # TODO: not complete functionality
-                    phoneme = G2P.rewriter(manual)[0]
-                    word2picked_phoneme[word] = phoneme
-
+            word2phonemized = json.loads(form["jsoned_word2phonemized"])
+            word2picked_phoneme = get_word2phoneme_from_front(word2phonemized, form)
             phonemized = phonemes_to_string(word2picked_phoneme.values())
     
         audio = timestamp_audio(synthesize_matcha_audio(text, phonemized))
+        jsoned_word2phonemized = json.dumps(word2phonemized, ensure_ascii=False)
 
         return render_template(
                 'text-to-audio.html', audio=audio,
                 text=text, word2phonemized=word2phonemized,
-                word2picked_phoneme=word2picked_phoneme
+                word2picked_phoneme=word2picked_phoneme,
+                jsoned_word2phonemized=jsoned_word2phonemized
             )
 
     return render_template('text-to-audio.html')
@@ -51,3 +44,25 @@ def timestamp_audio(filename):
     # so the browser caches the audio and doesn't update it (== plays the old audio)
     timestamp = str(int(time.time()))
     return f'{filename}?v{timestamp}'
+
+
+def get_word2phoneme_from_front(word2phonemized, form):
+    print(f'form: {form}')
+    print(f'word2phonemized: {word2phonemized}')
+    word2picked_phoneme = {}
+    for word in word2phonemized:
+        if not word.isalpha():
+            continue
+        phonemes = form.getlist(word)
+        picked, manual = phonemes
+        if not manual:          
+            word2picked_phoneme[word] = picked
+        else:
+            all_phonemes, new_phonemes = get_all_phonemes(
+                g2p=G2P, word=manual.lower(), current_phonemes=set(
+                    word2phonemized[word])
+            )
+            word2phonemized[word] = all_phonemes
+            word2picked_phoneme[word] = new_phonemes[0]
+    
+    return word2picked_phoneme
