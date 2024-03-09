68fa9b311321e8f15d0082969004079a4c363b3e
daschablume <daschablume@gmail.com>
2024-03-09 13:05:14 +0100

    implemented the ability to save phonemes

--- app/views.py
+++ app/views.py
@@ -7,12 +7,15 @@ from app.ipa_phonemizer import (
     convert_text_to_phonemized, phonemes_to_string,
     get_all_phonemes
 )
-#from app.db_utils import add_graphemes_and_log, fetch_grapheme2phoneme
 from app.load_models import G2P
 from app.matcha_utils import synthesize_matcha_audio
 
 
 def text_to_audio_view():
+    # import here, otherwise circular import
+    from app import db
+    from app.db_utils import add_graphemes_and_log
+
     if request.method == 'POST':
         form = request.form
         text = form['text']
@@ -21,6 +24,7 @@ def text_to_audio_view():
             phonemized, word2phonemized, word2picked_phoneme = (
                 convert_text_to_phonemized(text, G2P)
             )
+            word2picked_phoneme = replace_model_with_db(word2picked_phoneme)
             
         elif form.get('regenerate') or form.get('confirm'):
             print(form)
@@ -28,6 +32,15 @@ def text_to_audio_view():
             word2phonemized = json.loads(form["jsoned_word2phonemized"])
             word2picked_phoneme = get_word2phoneme_from_front(word2phonemized, form)
             phonemized = phonemes_to_string(word2picked_phoneme.values())
+
+            if form.get('confirm'):
+                try:
+                    add_graphemes_and_log(word2picked_phoneme)
+                    db.session.commit()
+                except Exception as e:
+                    # TODO: show the error to the user
+                    db.session.rollback()
+                    print(e)
 
         else:
             raise NotImplementedError
@@ -70,3 +83,15 @@ def get_word2phoneme_from_front(word2phonemized, form):
             word2picked_phoneme[word] = new_phonemes[0]
     
     return word2picked_phoneme
+
+
+def replace_model_with_db(word2model_phoneme: dict) -> dict:
+    '''
+    Replace the phonemes from the model with the phonemes from the db
+    '''
+    # import here, otherwise circular import
+    from app.db_utils import fetch_grapheme2phoneme
+    word2db_phoneme = fetch_grapheme2phoneme(list(word2model_phoneme.keys()))
+    for word, model_phoneme in word2model_phoneme.items():
+        word2model_phoneme[word] = word2db_phoneme.get(word, model_phoneme)
+    return word2model_phoneme
